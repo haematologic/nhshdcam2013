491e5db5c0c745aa9b94e6e8f1a069e49c57c383
Duncan Brian <haematologic+github@gmail.com>
2015-03-23 15:39:37 +0000

    add hsv based masking

--- python/patches/opencv/opencvhough1.py
+++ python/patches/opencv/opencvhough1.py
@@ -1,6 +1,7 @@
 from IPython import embed
 import cv2
 import numpy as np
+np.set_printoptions(threshold='nan')
 import time
 import math
 import sys
@@ -14,6 +15,44 @@ from os import listdir
 from os.path import isfile, join
 
 pi_4 = 4*math.pi
+
+def draw_hist(img):
+    ax = plt.subplot(3,1,1)
+    ax.set_title('H')
+    hist_hue = cv2.calcHist([img], [0], None, [180], [0,180])
+    ax.plot(hist_hue)
+
+    ax = plt.subplot(3,1,2)
+    ax.set_title('S')
+    hist_hue = cv2.calcHist([img], [1], None, [256], [0,255])
+    ax.plot(hist_hue)
+
+    ax = plt.subplot(3,1,3)
+    ax.set_title('V')
+    hist_hue = cv2.calcHist([img], [2], None, [256], [0,255])
+    ax.plot(hist_hue)
+    plt.ioff()
+    plt.show()
+    plt.ion()
+    #plt.show()
+
+def draw_hue(img):
+    ax = plt.subplot(1,1,1)
+    ax.set_title('H')
+    hist_hue = cv2.calcHist([img], [0], None, [99], [1,100])
+    ax.plot(hist_hue)
+    plt.ioff()
+    plt.draw()
+
+def find_hue_mid(img):
+    hist_hue = cv2.calcHist([img], [0], None, [99], [1,100])
+    hist_hue = np.array([i[0] for i in hist_hue.tolist()])
+    b = (np.diff(np.sign(np.diff(hist_hue))) > 0).nonzero()[0] + 1
+    print b
+    # Find non-zero min
+    idx = min([(i,hist_hue[i]) for i in b if hist_hue[i] > max(hist_hue)/100], key=lambda x: x[1])
+    print idx
+    return idx[0]
 
 def split_into_rgb_channels(image):
     
@@ -104,13 +143,15 @@ for image in images:
 
     img = cv2.imread(filename,0) # color =1, gray = 0, alpha = -1)
     imgcol = cv2.imread(filename)
+    imgcol1 = imgcol.copy()
+    
     # imgcol[:,:,2] = 0 
     colchannles = []
     colchannels = split_into_rgb_channels(imgcol)
     #gr, gg, gb = convert_rgb_channels_into_grays(red,green,blue)
     # img = cv2.cvtColor(imgcol,cv2.COLOR_BGR2GRAY) 
     imgcol = cv2.cvtColor(imgcol,cv2.COLOR_BGR2RGB)	
-     
+    imgcol2 = imgcol.copy()
     if img==None:
         print "cannot open ",filename
 
@@ -122,6 +163,35 @@ for image in images:
 
             img = colchannels[j]
             blur = cv2.GaussianBlur(img,(3,3),0)
+            
+            # convert to bgr to hsv
+            #imgcol1 = cv2.GaussianBlur(imgcol1,(3,3),0)
+            hsv = cv2.cvtColor(imgcol1, cv2.COLOR_BGR2HSV)
+           
+            #draw_hue(hsv)
+            # define range of colors in HSV
+            lower_red1 = np.array([155,100,50], dtype=np.uint8)
+            upper_red1 = np.array([180,255,255], dtype=np.uint8)
+            lower_red2 = np.array([0,90,40], dtype=np.uint8)
+            upper_red2 = np.array([10,255,255], dtype=np.uint8)
+            lower_green = np.array([50,100,50], dtype=np.uint8)
+            upper_green = np.array([80,255,255], dtype=np.uint8)
+            lower_aqua = np.array([91,110,100], dtype=np.uint8)
+            upper_aqua = np.array([115,255,255], dtype=np.uint8)
+            lower_gold = np.array([12,10,25], dtype=np.uint8)
+            upper_gold = np.array([40,130,255], dtype=np.uint8)
+            lower_yellow = np.array([15,131,50], dtype=np.uint8)
+            upper_yellow = np.array([28,255,255], dtype=np.uint8)
+             
+        
+            mask_red1 = cv2.inRange(hsv, lower_red1, upper_red1)
+            mask_red2 = cv2.inRange(hsv, lower_red2, upper_red2)
+            mask_red = cv2.bitwise_or(mask_red1, mask_red2)
+            mask_green = cv2.inRange(hsv, lower_green, upper_green)
+            mask_aqua = cv2.inRange(hsv, lower_aqua, upper_aqua)
+            mask_gold = cv2.inRange(hsv, lower_gold, upper_gold)
+            mask_yellow = cv2.inRange(hsv, lower_yellow, upper_yellow)
+            
             
             #border = img - cv2.erode(img, None)
             #plt.subplot(2,2,2),plt.imshow(border)
@@ -156,6 +226,8 @@ for image in images:
                 thresh = i
 
             # find otsu's threshold value with OpenCV function
+            blur = cv2.erode(blur,None,iterations = 1)
+            
             ret, otsu = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
             print j, thresh, ret
             labelarray, count = label(otsu)
@@ -178,7 +250,10 @@ for image in images:
             sure_bg = cv2.dilate(opening,kernel,iterations=3)
             
             # find sure foreground area
-            dist_transform = cv2.distanceTransform(opening,cv2.cv.CV_DIST_L2,5)
+            #dist_transform = cv2.distanceTransform(opening,cv2.cv.CV_DIST_L2,5)
+            dist_transform = cv2.distanceTransform(
+                opening,cv2.cv.CV_DIST_L2, cv2.cv.CV_DIST_MASK_PRECISE)
+            #print (dist_transform)
             retsfg, sure_fg = cv2.threshold(
                 dist_transform,0.7*dist_transform.max(),255,0)
             
@@ -233,17 +308,25 @@ for image in images:
             #plt.figure(figsize=(8,8))
             imgcolz = cv2.cvtColor(imgcol, cv2.COLOR_BGR2RGB)
             
-            plt.subplot(2,2,1),plt.imshow(imgcol)
+            plt.subplot(3,2,1),plt.imshow(imgcol2)
             plt.title('%s-input ch%d' % (filename, j))
              
-            plt.subplot(2,2,2),plt.imshow(img,'gray')
-            plt.title('ch%d filt-input' % (j))
-            plt.subplot(2,2,3),plt.imshow(labelarray1)
-            plt.title('ch%d labelarray1' % (j))
+            plt.subplot(3,2,2),plt.imshow(mask_red)
+            plt.title('ch%d mask_red' % (j))
+            plt.subplot(3,2,3),plt.imshow(mask_green)
+            plt.title('ch%d mask green' % (j))
+            plt.subplot(3,2,4),plt.imshow(mask_aqua)
+            plt.title('ch%d mask aqua' % (j))
+            plt.subplot(3,2,5),plt.imshow(mask_gold)
+            plt.title('ch%d mask gold' % (j))
+            plt.subplot(3,2,6),plt.imshow(mask_yellow)
+            plt.title('ch%d mask yellow' % (j))
+            #plt.subplot(2,2,3),plt.imshow(labelarray1)
+            #plt.title('ch%d labelarray1' % (j))
             #plt.subplot(2,2,4),plt.imshow(mh.overlay(img,rmax))
             #plt.title('ch%d mh.rmax' % (j))
-            plt.subplot(2,2,4),plt.imshow(opening)
-            plt.title('ch%d opening' % (j))
+            #plt.subplot(2,2,4),plt.imshow(opening)
+            #plt.title('ch%d opening' % (j))
             plt.ion()
             plt.draw()
             
