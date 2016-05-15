568595b6cadc56739d1e344a9384a3f31362af25
Nick Powell <nicholas.powell.11@ucl.ac.uk>
2016-05-15 12:17:00 +0100

    Added HistogramFeature.cs and Cluster.cs

---
+++ PatchClustering/PatchClustering/CellPatchClustering/Cluster.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CellPatchClustering
+{
+    /// <summary>
+    /// A cluster of patches.
+    /// </summary>
+    public class Cluster
+    {
+        public int Count { get; set; }
+
+        public List<Patch> SamplePatches { get; set; }
+    }
+}
---
+++ PatchClustering/PatchClustering/CellPatchClustering/HistogramFeature.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CellPatchClustering
+{
+    public class HistogramFeature : IFeature
+    {
+        public int Bin {get;set;} // 0..number of bins
+
+        public int Threshold { get; set; }
+
+        public byte Channel { get; set; } // between 0 (red) and 2 (blue)
+
+        public bool ComputeFeature(Patch p)
+        {
+            return p.Histogram[Channel][Bin] > Threshold;
+        }
+
+        public override string ToString()
+        {
+            return "Histogram["+Channel+","+Bin+">"+Threshold+"]";
+        }
+
+        public void Sample(Random rnd, Patch p)
+        {
+            Bin = rnd.Next(p.Histogram[0].Length);
+            Threshold = rnd.Next(p.Width * p.Height);
+            Channel = (byte)rnd.Next(3);
+        }
+    }
+}
